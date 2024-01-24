7e4297aaf060d0128cff55d4606b2fd8fad2e599
sunilgawai <sunilgawai7397@gmail.com>
2024-01-25 02:08:31 +0530

    site config updated

--- src/config/site.ts
+++ src/config/site.ts
@@ -10,8 +10,8 @@ export const siteConfig = {
       href: "/",
     },
     {
-      title: "Products",
-      href: "/products",
+      title: "Menus",
+      href: "/menus",
     },
     {
       title: "Cart",
