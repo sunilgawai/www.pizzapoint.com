b6c0072b4bd87a9b9a3dee98ad13d7303261648c
sunilgawai <sunilgawai7397@gmail.com>
2024-01-27 19:58:03 +0530

    app table addded

---
+++ src/components/tables/app-table.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { DataTable } from "@/components/ui/data-table";
+import { Separator } from "@/components/ui/separator";
+import { User } from "@/constants/data";
+import { useRouter } from "next/navigation";
+import { columns } from "./users-column";
+
+interface ProductsClientProps {
+    data: User[];
+}
+
+const UsersTable: React.FC<ProductsClientProps> = ({ data }) => {
+    const router = useRouter();
+
+    return (
+        <>
+            <div className="flex items-start justify-between">
+            </div>
+            <Separator />
+            <DataTable searchKey="name" columns={columns} data={data} />
+        </>
+    );
+};
+
+export default UsersTable;
