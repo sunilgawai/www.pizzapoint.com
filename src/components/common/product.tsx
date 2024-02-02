c3308d054d2fe9b2eab6f6e1c5077fd48aa46a91
sunilgawai <sunilgawai7397@gmail.com>
2024-02-02 16:07:00 +0530

    price & description added

--- src/components/common/product.tsx
+++ src/components/common/product.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image'
 import demoImg from '@/assets/corousel-img-1.jpg'
 import { Button } from '../ui/button'
 import Link from "next/link";
+import { Separator } from '../ui/separator'
 
 const Product = () => {
     return (
         <Link href={"/products"}>
-            <Card className='py-2 rounded-none border-none'>
+            <Card className='pt-2 rounded-none border-none'>
                 <CardContent className='w-full'>
                     <Image src={demoImg} alt='demo' className='hover:animate-in' />
                     <CardTitle className='py-3 font-semibold text-base'>
@@ -19,8 +20,14 @@ const Product = () => {
                     </CardDescription>
                 </CardContent>
                 <CardFooter className='flex items-center justify-between'>
+                    <div className="flex flex-col text-xl font-semibold">
+                        <div className=' flex flex-row'>
+                            <h1 className='text-xl font-semibold'>$999</h1>
+                            <span className="line-through text-sm font-normal ml-4">$20</span>
+                        </div>
+                        <h2 className='text-sm mt-2 font-semibold'>50% OFF</h2>
+                    </div>
                     <Button variant={"outline"} className='border border-gray-300' size="sm">Add to cart</Button>
-                    <Button variant={"outline"} className='border border-gray-300' size="sm">Buy Now</Button>
                 </CardFooter>
             </Card>
         </Link>
